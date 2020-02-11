a35dc49fde31f5383228b74857c36daf1eafa3d7
richknicks <rlynch@gmail.com>
2020-02-11 16:03:56 -0500

    set up axios

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
-
+import axios from 'axios';
 import './App.css';
 
-function App() {
+ class App extends React.Component {
+   state = {
+     user:[],
+     followers:[]
+   };
+
+   componentDidMount(){
+     axios
+     .get('https://api.github.com/users/richknicks')
+     .then(response =>
+      console.log (response))
+     .catch(error =>
+      console.log(error))
+   }
+
+
+
+
+
+
+  render (){
   return (
     <div className="App">
       
     </div>
   );
 }
-
+ }
 export default App;
