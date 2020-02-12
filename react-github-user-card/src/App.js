2e174ed50883d9bdf41f3bd6775378c7d03ab463
richknicks <rlynch@gmail.com>
2020-02-12 10:54:32 -0500

    added logo link for github

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
 import axios from "axios";
-// import Followers from './Followers';
 import "./App.css";
+import styled from 'styled-components';
+
+const MyDiv = styled.div`
+display: flex;
+flex-direction: column;
+margin: 0 auto;
+width: 50%;
+`;
+const MyImg = styled.img`
+border-radius: 12px;
+width: 50%;
+`;
+const MyPtag = styled.p`
+font-size: 1.2rem;
+
+`;
+
 
 class App extends React.Component {
   state = {
@@ -37,23 +53,26 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
+      <MyDiv className="App">
+        <div>
         <h1>GitHub User</h1>
-        <img src={this.state.user.avatar_url} />
-        <p>Name: {this.state.user.name}</p>
-        <p>Location: {this.state.user.location}</p>
-        <p>Number or followers: {this.state.user.followers}</p>
-        <p>Number or following: {this.state.user.following}</p>
+        <MyImg src={this.state.user.avatar_url} />
+        <h2>Name: {this.state.user.name}</h2>
+        <MyPtag>Location: {this.state.user.location}</MyPtag>
+        <MyPtag>Number or followers: {this.state.user.followers}</MyPtag>
+        <MyPtag>Number or following: {this.state.user.following}</MyPtag>
+        </div>
         {this.state.followers.map(person => {
           return (
-            <>
+            <div>
             <h1>GitHub Follower</h1>
-              <img src={person.avatar_url} />
-              <p>Name: {person.login}</p>
-            </>
+              <MyImg src={person.avatar_url} />
+              <h2>Name: {person.login}</h2>
+              <a href={person.html_url}><img src={`/images/Github-resize.png`} alt="Github logo"/></a>
+            </div>
           );
         })}
-      </div>
+      </MyDiv>
     );
   }
 }
